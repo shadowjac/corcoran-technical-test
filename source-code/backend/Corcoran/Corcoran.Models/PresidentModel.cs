e318796c581412ce7f8b851e71a5711f049bc65b
Andres <ARKANTOS_JAC@hotmail.com>
2018-09-16 16:51:30 -0500

    Adding json serializer support

--- source-code/backend/Corcoran/Corcoran.Models/PresidentModel.cs
+++ source-code/backend/Corcoran/Corcoran.Models/PresidentModel.cs
@@ -1,13 +1,19 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace Corcoran.Models
 {
     public class PresidentModel
     {
+        [JsonProperty("name")]
         public string Name { get; set; }
+        [JsonProperty("birthDate")]
         public DateTime BirthDate{ get; set; }
+        [JsonProperty("birthPlace")]
         public string BirthPlace { get; set; }
+        [JsonProperty("deathDate")]
         public DateTime? DeathDate { get; set; }
+        [JsonProperty("deathPlace")]
         public string DeathPlace { get; set; }
     }
 }
